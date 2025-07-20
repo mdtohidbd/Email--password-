84179603026243b736dd17f233985484ecf72bee
mdtohidvai <mdtohid242024@gmail.com>
2025-07-20 23:00:35 +0600

    lonin page update

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Login = () => {
-    return (
-        <div>
-            <h2 className="text-4xl">Login</h2>
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+    // Login logic goes here
+  };
+
+  
+
+  return (
+    <div className="">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <form onSubmit={handleLogin}>
+              <label className="label">Email</label>
+              <input
+                type="email"
+                className="input input-bordered"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+
+              <label className="label">Password</label>
+              <input
+                type="password"
+                className="input input-bordered"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+
+              <div>
+                <a href="#" className="link link-hover">Forgot password?</a>
+              </div>
+
+              <button type="submit" className="btn btn-neutral mt-4">Login</button>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
